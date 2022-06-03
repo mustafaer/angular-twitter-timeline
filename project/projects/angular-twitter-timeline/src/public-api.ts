9d39347f919bb458b4c9b9f36a3c2ecf3c395a6c
Mustafa ER <mustafaerpro@gmail.com>
2022-06-03 19:47:10 +0300

    unused folder removed

--- project/projects/angular-twitter-timeline/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of angular-twitter-timeline
- */
-
-export * from './lib/angular-twitter-timeline.service';
-export * from './lib/angular-twitter-timeline.component';
-export * from './lib/angular-twitter-timeline.module';
